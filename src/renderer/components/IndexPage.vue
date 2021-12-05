eafbc750792f87fb50978c9960ec42b9b28efb62
ofg <1309870473@qq.com>
2021-12-05 16:58:29 +0800

    init

---
+++ src/renderer/components/IndexPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Input v-model="value" placeholder="Enter something..." style="width: 300px" />
+    <Icon type="ios-checkmark" />
+  </div> 
+</template>
+
+<script>
+
+  export default {
+    name: 'index-page',
+    methods: {
+      
+    },
+    data(){
+      return {
+        value: 1
+      }
+    }
+  }
+</script>
+
+<style>
+
+  
+</style>
